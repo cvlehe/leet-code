f93fe7d7aea033f51e7b91f12b78e696e3d0ac48
Charlie von Lehe <vonlehec@gmail.com>
2023-12-29 12:28:51 -0500

    Time: 106 ms (5.16%) | Memory: 42.8 MB (52.26%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.ts
+++ 2556-convert-the-temperature/convert-the-temperature.ts
@@ -1,5 +1,3 @@
 function convertTemperature(celsius: number): number[] {
-    const k = celsius + 273.15;
-    const f = celsius * 1.8 + 32;
-    return [k, f];
+    return [celsius + 273.15, celsius * 1.8 + 32];
 };
