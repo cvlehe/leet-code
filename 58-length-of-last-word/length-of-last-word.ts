cf97aeea0dd76670fed0be0f4f267663cb9f04e5
Charlie von Lehe <vonlehec@gmail.com>
2024-02-16 13:19:21 -0500

    Time: 63 ms (24.57%) | Memory: 51.5 MB (9.68%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.ts
+++ 58-length-of-last-word/length-of-last-word.ts
@@ -1,18 +1,3 @@
 function lengthOfLastWord(s: string): number {
-    let length = 0;
-    let currentLength = 0;
-
-    for(var i = 0; i < s.length; i++) {
-        if(s[i] !== " ") {
-            currentLength++;
-        }
-        if((i + 1 < s.length && s[i + 1] === " ") || i + 1 === s.length) {
-            if (currentLength > 0) {
-                console.log(currentLength);
-                length = currentLength;
-            }
-            currentLength = 0;
-        }
-    }
-    return length;
+    return s.split(" ").filter(s => s.length > 0).slice(-1)[0].length;
 };
