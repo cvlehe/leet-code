c3ed917b87fee3686aab513e5dd653ae5ffabec1
Charlie von Lehe <vonlehec@gmail.com>
2024-02-21 10:54:19 -0500

    Time: 62 ms (70.39%) | Memory: 53.3 MB (55.91%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -13,13 +13,5 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-    return getDepth(root, 0);
+    return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0;
 };
-
-function getDepth(root: TreeNode, depth: number): number {
-    if (!root) {
-        return depth;
-    }
-    depth++;
-    return Math.max(getDepth(root.left, depth), getDepth(root.right, depth));
-}
